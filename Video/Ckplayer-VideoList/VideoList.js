b04a1a59033c370fcd89904f581039b66959eaf6
SUN-SZY <1224430842@qq.com>
2022-03-10 08:31:25 +0800

    Signed-off-by: SUN-SZY <1224430842@qq.com>

---
+++ Video/Ckplayer-VideoList/VideoList.js
@@ -0,0 +1,12 @@
+var videoObject = {
+    container: '.video',
+    poster: 'https://cdn.seovx.com/ha/?mom=302',
+    video: 'https://ckplayer-video.oss-cn-shanghai.aliyuncs.com/mp4/1_640x480.mp4'
+};
+var player = new ckplayer(videoObject)
+var videoObject1 = {
+    container: '.video1',
+    poster: 'https://cdn.seovx.com/ha/?mom=302',
+    video: 'https://ckplayer-video.oss-cn-shanghai.aliyuncs.com/mp4/1_640x480.mp4'
+};
+var player = new ckplayer(videoObject1)
